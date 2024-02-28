a0735cc337ed488f53134a96e86834d07dc97f3c
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2024-02-28 14:48:00 +0100

    works

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-minikube kubectl -- apply -f schoodle-deployment.yml
-minikube kubectl -- apply -f schoodle-service.yml
-minikube kubectl -- apply -f schoodle-ingress.yml
+minikube kubectl -- apply -f schoodle-api-deployment.yml
